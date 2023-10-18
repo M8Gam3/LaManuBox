4d33d8b87e8893facfc14ec82ad534c8a90dc595
BenjaminF <benji-fo@outlook.fr>
2023-10-18 20:00:05 +0200

    start-header

--- LaManuBox/src/components/Header/Header.jsx
+++ LaManuBox/src/components/Header/Header.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+
+
 export function Header() {
     return (
-        <></>
+        <>
+            <div className="header">
+                <img src="" alt="Logo-LaManuBox" />
+                <nav>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                </nav>
+            </div>
+            <div className="hero-banner">
+                <h1>LaManuBox</h1>
+                <h2>Phrase d'accroche bien longue et je rajoute des mots car je sais pas quoi mettre à la place</h2>
+            </div>
+        </>
     )
 }
