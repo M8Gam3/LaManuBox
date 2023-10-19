8a9de77aa928b39fb03647861277bc0c796a0fa0
Théo <64200915+Magic-80@users.noreply.github.com>
2023-10-19 08:42:05 +0200

    Update login create account

--- LaManuBox/src/pages/Login.jsx
+++ LaManuBox/src/pages/Login.jsx
@@ -1,7 +1,57 @@
-﻿export function Login() {
+﻿import React, { useRef, useState } from "react";
+
+export function Login() {
+
+    const email = useRef();
+    const password = useRef();
+
+
+    const registerUser = () => {
+        if(email.current.value && password.current.value)
+        {
+            localStorage.setItem("email" , email.current.value)
+            localStorage.setItem("password" , password.current.value)
+        }
+    }
+
+
+    const login = () =>{
+        if(localemail && localpassword)
+        {
+            console.log("login réussie");
+        }
+    }
+
     return (
         <>
-            <h1> Login </h1>
+                      
+            <div className="login">
+                <p> Connextion </p>
+                <form className="form" action="">
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" /> 
+                     <button type="button" onClick={login}> Login </button>
+                    <div className="remember">
+                        <input type="checkbox" />
+                        <label htmlFor="remember"> Remember me  </label>
+                    </div>
+                    <p> forget your passaword </p>
+                </form>
+            </div>
+
+            <div className="login">
+                <p> Incription </p>
+                <form className="form" action="">
+                    <input type="email" placeholder="Email" ref={email} />
+                    <input type="password" placeholder="Password" ref={password} /> 
+                     <button type="submit" onClick={registerUser}> create acount </button>
+                    <div className="remember">
+                        <input type="checkbox" />
+                        <label htmlFor="remember"> Remember me  </label>
+                    </div>
+                    <p> forget your passaword </p>
+                </form>
+            </div>
         </>
     )
 }
