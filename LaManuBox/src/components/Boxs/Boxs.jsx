5ea86a19027cbbcd5493985e1006caf54e9f82cc
Marius Hazart <115085694+M8Gam3@users.noreply.github.com>
2023-10-23 10:23:04 +0200

    push

--- LaManuBox/src/components/Boxs/Boxs.jsx
+++ LaManuBox/src/components/Boxs/Boxs.jsx
@@ -4,25 +4,42 @@ import './Boxs.scss'
 export default function Boxs() {
     return (
         <>
-            <section id='Boxs' className='boxs'>
+            <section id='Boxs' className='boxs_section'>
                 <div className="boxs_container">
                     <div className="boxs_products">
-                        <div className="boxs_products-1">
-                            <p className='boxs_products_title'>BOX 1</p>
+                        <div className="boxs_product boxs_product-1">
+                            <div className='boxs_products_title'>
+                                <h3>BOX S</h3>
+                            </div>
+                            <div className='boxs_products_subtitle'>
+                                <p>2 à 3 m²</p>
+                            </div>
                         </div>
-                        <div className="boxs_products-2">
-                            <p className='boxs_products_title'>BOX 2</p>
+                        <div className="boxs_product boxs_product-2">
+                            <div className='boxs_products_title'>
+                                <h3>BOX M</h3>
+                            </div>
+                            <div className='boxs_products_subtitle'>
+                                <p>3 à 6 m²</p>
+                            </div>
                         </div>
-                        <div className="boxs_products-3">
-                            <p className='boxs_products_title'>BOX 3</p>
+                        <div className="boxs_product boxs_product-3">
+                            <div className='boxs_products_title'>
+                                <h3>BOX L</h3>
+                            </div>
+                            <div className='boxs_products_subtitle'>
+                                <p>6 à 10 m²</p>
+                            </div>
                         </div>
-                        <div className="boxs_products-4">
-                            <p className='boxs_products_title'>BOX 4</p>
+                        <div className="boxs_product boxs_product-4">
+                            <div className='boxs_products_title'>
+                                <h3>BOX XL</h3>
+                            </div>
+                            <div className='boxs_products_subtitle'>
+                                <p>10 à 16 m²</p>
+                            </div>
                         </div>
                     </div>
-                    {/* <div className='boxs_img'>
-                        <img src={scrap_paper3} alt="scrap-paper-3" />
-                    </div> */}
                 </div>
             </section>
         </>
